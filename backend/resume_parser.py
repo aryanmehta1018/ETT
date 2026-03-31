24fc86993f2757aab96a9a4941859513fa5a33d2
ajiteshshuklaa <AJITESH.23FE10CSE00130@muj.manipal.edu>
2026-03-31 08:56:57 +0530

    Update resume_parser.py

--- backend/resume_parser.py
+++ backend/resume_parser.py
@@ -1,1 +1,18 @@
+import pdfplumber
+from docx import Document
+
+def extract_text(file):
+
+    if file.name.endswith(".pdf"):
+        text = ""
+        with pdfplumber.open(file) as pdf:
+            for page in pdf.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text
+        return text
+
+    elif file.name.endswith(".docx"):
+        doc = Document(file)
+        return "\n".join([para.text for para in doc.paragraphs])
 
